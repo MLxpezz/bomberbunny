73682a42894974e2568660e46315e5fdfbbd287c
MLxpezz <mauricio_lopez_098@hotmail.com>
2024-12-15 16:46:27 -0600

    first commit

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var speed = 300.0;
+
+@onready var animation: AnimatedSprite2D = $"AnimatedSprite2D";
+
+func _ready() -> void:
+	animation.play("idle");
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+
+func movement() -> void:
+	pass
